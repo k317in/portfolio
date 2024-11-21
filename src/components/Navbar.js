55725c28a0661073542f9ab5736cd209e572b041
k317in <k317in@gmail.com>
2024-11-21 16:06:52 +0000

    updated Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => (
+    <nav>
+      <h1>My Portfolio</h1>
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+  export default Navbar;  
