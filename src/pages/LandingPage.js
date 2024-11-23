2f9c92f61540d1e3fb714b0d5b3ffd6f1ac00bee
k317in <k317in@gmail.com>
2024-11-23 22:09:06 +0000

    landing page css update

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LandingPage.css";
 
 const LandingPage = () => {
+  const [fadeOut, setFadeOut] = useState(false);
+
+  const handleTouch = () => {
+    setFadeOut(true); // Trigger fade-out animation
+    setTimeout(() => {
+      window.location.href = "/home"; // Navigate to main page after animation
+    }, 1000); // Match the duration of the fade-out animation
+  };
+
   return (
-    <div className="landing-container">
+    <div
+      className={`landing-container ${fadeOut ? "fade-out" : ""}`}
+      onClick={handleTouch}
+    >
       <div className="landing-content">
-        <h1>Welcome to My Portfolio</h1>
-        <p>Your journey into my world of web development starts here.</p>
-        <button
-          className="landing-button"
-          onClick={() => (window.location.href = "/home")}
-        >
-          Enter
-        </button>
+        <div className="landing-logo">
+            <h1>K</h1><h1 className="landing-logo-3">3</h1><h1 className="landing-logo-1">1</h1><h1 className="landing-logo-7">7</h1><h1>IN</h1>
+            </div>
+        <p>The journey into my world.</p>
+      </div>
+
+      <div className="landing-content2">
+      <p>TOUCH ANYWHERE TO START</p>
       </div>
     </div>
   );
